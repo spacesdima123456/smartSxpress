001ffeb8c43d49ae8b878ae947e758d1b50cfbdf
Dima <spaces_dima@mail.ru>
2021-08-30 19:54:46 +0600

    Renamed folder wpf3 on Wms.

--- Wms/Admin.xaml.cs
+++ Wms/Admin.xaml.cs
@@ -9,9 +9,9 @@ namespace Wms
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Admin : Window
     {
-        public MainWindow()
+        public Admin()
         {
             InitializeComponent();
         }
