296c9293faba899a7da62f4606c95cef6da286d9
Dima <spaces_dima@mail.ru>
2021-08-30 20:07:36 +0600

    Refactor login.xaml.

--- Wms/ViewModel/LoginViewModel.cs
+++ Wms/ViewModel/LoginViewModel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Wms.ViewModel
+﻿namespace Wms.ViewModel
 {
-    class LoginViewModel
+    public class LoginViewModel: BaseViewModel
     {
+
     }
 }
