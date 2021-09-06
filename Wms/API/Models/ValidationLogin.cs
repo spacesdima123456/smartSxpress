ef563f4ae50a562af055111da8204571077a90fc
Дима <spaces_dima@mail.ru>
2021-09-06 15:36:54 +0600

    Add attribyte text.

--- Wms/API/Models/ValidationLogin.cs
+++ Wms/API/Models/ValidationLogin.cs
@@ -5,6 +5,7 @@ namespace Wms.API.Models
     public class ValidationLogin
     {
         private string _text;
+        [JsonPropertyName("text")]
         public string Text
         {
             get => Code != 2 ? _text : "";
