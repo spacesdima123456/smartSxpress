056ac961d2d7b61f431b71f319e52d4c77be4038
Дима <spaces_dima@mail.ru>
2021-09-10 11:52:40 +0600

    Add models api for branch.

---
+++ Wms/API/Models/Branch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Wms.API.Models
+{
+    public class Branch
+    {
+        [JsonPropertyName("code")]
+        public int Code { get; set; }
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+        [JsonPropertyName("branch")]
+        public List<Branches> Branches { get; set; }
+    }
+}
---
+++ Wms/API/Models/Branches.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Wms.API.Models
+{
+  public  class Branches
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("countryСode")]
+        public string CountryCode { get; set; }
+        [JsonPropertyName("company")]
+        public string Company { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+        [JsonPropertyName("address")]
+        public string Address { get; set; }
+        [JsonPropertyName("city")]
+        public string City { get; set; }
+        [JsonPropertyName("state")]
+        public string State { get; set; }
+        [JsonPropertyName("zip")]
+        public int Zip { get; set; }
+        [JsonPropertyName("phone")]
+        public string Phone { get; set; }
+        [JsonPropertyName("logo")]
+        public string Logo { get; set; }
+        [JsonPropertyName("language")]
+        public string Language { get; set; }
+    }
+}
