c52c6b0469c8c00034ffc9b9f8749b94f5d36f38
Дима <spaces_dima@mail.ru>
2021-09-13 16:45:47 +0600

    Centered the confirm of deleting the branch relative to the parent window.

--- Wms/WindowDialog/BranchDelete.xaml.cs
+++ Wms/WindowDialog/BranchDelete.xaml.cs
@@ -9,9 +9,21 @@ namespace Wms.WindowDialog
         public BranchDelete(Action<object> action)
         {
             InitializeComponent();
+            SetWindowStartupLocation();
             DataContext = new BranchDeleteViewModel(action);
         }
 
         private void Close(object sender, RoutedEventArgs e) => Close();
+
+        private void SetWindowStartupLocation()
+        {
+            var curApp = Application.Current;
+            var mainWindow = curApp.MainWindow;
+            if (mainWindow!=null)
+            {
+                Left = mainWindow.Left + (mainWindow.Width - ActualWidth) / 2;
+                Top = mainWindow.Top + (mainWindow.Height - ActualHeight) / 2;
+            }
+        }
     }
 }
