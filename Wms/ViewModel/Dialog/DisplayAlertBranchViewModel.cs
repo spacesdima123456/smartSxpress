28b68b0668bef5610929ad9d5dcf3bcea91ceba1
Дима <spaces_dima@mail.ru>
2021-09-27 14:53:16 +0600

    Disable combobox country.

--- Wms/ViewModel/Dialog/DisplayAlertBranchViewModel.cs
+++ Wms/ViewModel/Dialog/DisplayAlertBranchViewModel.cs
@@ -4,17 +4,17 @@ using System.Windows;
 
 namespace Wms.ViewModel.Dialog
 {
-    public class DisplayAlertBranchViewModel: DisplayAlertBranchBaseViewModel
+    public class DisplayAlertBranchViewModel : DisplayAlertBranchBaseViewModel
     {
-
-        public DisplayAlertBranchViewModel(Action<DisplayAlertBranchBaseViewModel> action):base(action)
+        public DisplayAlertBranchViewModel(Action<DisplayAlertBranchBaseViewModel> action) : base(action)
         {
             InitProperty(480, "Create", true, null, string.Empty, string.Empty, string.Empty,
                 string.Empty, string.Empty, App.Data.Data.Customer.Company, string.Empty, Visibility.Visible, "Create",
-                string.Empty);
+                App.Data.Data.Customer.CountryName);
         }
 
-        public DisplayAlertBranchViewModel(Branches branches, Action<DisplayAlertBranchBaseViewModel> action) : base(action)
+        public DisplayAlertBranchViewModel(Branches branches, Action<DisplayAlertBranchBaseViewModel> action) :
+            base(action)
         {
             InitProperty(395, "Edit", false, branches.Zip, branches.Name, branches.City, branches.Phone,
                 branches.State, branches.Email, branches.Company, branches.Address, Visibility.Collapsed, "Done",
